82858a106b15cf4f5495cd2eb4fbc23cb30448b4
LeezeeGD <elizabethuwakwe@gmail.com>
2021-08-03 15:23:34 +0100

    making sense

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-const burger = document.querySelector('')
+const burger = document.querySelector('.hamburger')
+const closebtn = document.querySelector('.close-btn')
+const nav = document.querySelector('nav')
+burger.addEventListener ('click', openNav)
+closebtn.addEventListener('click', closeNav)
+
+function openNav(e) {
+    e.preventDefault()
+    nav.style.display='block'
+}
+
+function closeNav(e) {
+    e.preventDefault()
+    nav.style.display='none'
+}
